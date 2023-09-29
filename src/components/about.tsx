1c5d8e3d2387aac762ff781dd501564a35fb8322
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2023-09-29 11:35:52 +0200

    [improvement] - Improved files structure

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,9 +3,15 @@
 import { useSectionInView } from '@/lib/hooks';
 import { motion } from 'framer-motion';
 import SectionHeading from './section-heading';
+import Link from 'next/link';
+import { BsArrowRight, BsGithub } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { RiLinkedinFill } from 'react-icons/ri';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 const About = () => {
   const { ref } = useSectionInView('About');
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <motion.section
@@ -17,24 +23,71 @@ const About = () => {
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
-      <p className="mb-3 uppercase">
+      <p>
         When I discovered programming, I was immediately drawn to the artistic aspect: the ability
         to make something out of nothing. Programming allows me to think outside the box, be
         creative and impact the world on a larger scale.
       </p>
       <br />
-      <p className="uppercase">
+      <p>
         In the past I have worked on projects related to the development and implementation of
         various e-commerce websites for companies ranging from start-ups to established companies. I
         have experience developing and implementing solutions to help these companies build and
         market their own web-based products and strengthen their brand identity.
       </p>
       <br />
-      <p className="uppercase">
+      <p>
         Always looking for new and better approaches and technologies that help me make a bigger
         impact and innovate. As an entrepreneurial creator, I see new opportunities every day. New
         technologies are added regularly that I need to master in order to succeed.
       </p>
+      <motion.div
+        className="flex gap-4 px-4 text-lg font-medium sm:flex-row mt-10"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.2,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="flex items-center gap-2 py-3 font-medium text-gray-500 transition rounded-full outline-none cursor-pointer group borderBlack px-7 focus:outline-none focus:scale-110 hover:scale-110 active:scale-105 dark:border-white/10"
+          onClick={() => {
+            setActiveSection('Contact');
+            setTimeOfLastClick(Date.now());
+          }}
+        >
+          CONTACT ME HERE{' '}
+          <BsArrowRight className="transition opacity-70 group-hover:translate-x-1" />
+        </Link>
+
+        <a
+          className="group flex items-center gap-2 bg-[#d0cfc5] text-gray-500 px-7 py-3 rounded-full font-medium
+        outline-none focus:outline-none focus:scale-110 hover:scale-110 active:scale-105 cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="/CV.pdf"
+          download
+        >
+          DOWNLOAD CV <HiDownload className="transition opacity-80 group-hover:translate-y-1" />
+        </a>
+
+        <a
+          className="flex items-center bg-[#d0cfc5] p-4 text-gray-500 hover:text-gray-950 gap-2 rounded-full cursor-pointer focus:scale-115 hover:scale-110 active:scale-105 borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/giovanni-hislop/"
+          target="_blank"
+          title="My LinkedIn profile"
+        >
+          <RiLinkedinFill />
+        </a>
+
+        <a
+          className="flex items-center bg-[#d0cfc5] p-4 text-gray-500 hover:text-gray-950 gap-2 rounded-full cursor-pointer focus:scale-115 hover:scale-110 active:scale-105 borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/gmhislop"
+          target="_blank"
+          title="My GitHub profile"
+        >
+          <BsGithub />
+        </a>
+      </motion.div>
     </motion.section>
   );
 };
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,11 +3,6 @@
 import { useActiveSectionContext } from '@/context/active-section-context';
 import { useSectionInView } from '@/lib/hooks';
 import { motion } from 'framer-motion';
-import Image from 'next/image';
-import Link from 'next/link';
-import { BsArrowRight, BsGithub } from 'react-icons/bs';
-import { HiDownload } from 'react-icons/hi';
-import { RiLinkedinFill } from 'react-icons/ri';
 
 const Intro = () => {
   const { ref } = useSectionInView('Home');
@@ -24,54 +19,6 @@ const Intro = () => {
         <span className="font-bold">frontend developer</span> with{' '}
         <span className="font-bold">{new Date().getFullYear() - 2021} years</span> of experience.
       </motion.h1>
-
-      <motion.div
-        className="flex gap-4 px-4 text-lg font-medium sm:flex-row"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.2,
-        }}
-      >
-        <Link
-          href="#contact"
-          className="flex items-center gap-2 py-3 font-medium text-gray-500 transition rounded-full outline-none cursor-pointer group borderBlack px-7 focus:outline-none focus:scale-110 hover:scale-110 active:scale-105 dark:border-white/10"
-          onClick={() => {
-            setActiveSection('Contact');
-            setTimeOfLastClick(Date.now());
-          }}
-        >
-          CONTACT ME HERE{' '}
-          <BsArrowRight className="transition opacity-70 group-hover:translate-x-1" />
-        </Link>
-
-        <a
-          className="group flex items-center gap-2 bg-[#d0cfc5] text-gray-500 px-7 py-3 rounded-full font-medium
-        outline-none focus:outline-none focus:scale-110 hover:scale-110 active:scale-105 cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="/CV.pdf"
-          download
-        >
-          DOWNLOAD CV <HiDownload className="transition opacity-80 group-hover:translate-y-1" />
-        </a>
-
-        <a
-          className="flex items-center bg-[#d0cfc5] p-4 text-gray-500 hover:text-gray-950 gap-2 rounded-full cursor-pointer focus:scale-115 hover:scale-110 active:scale-105 borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/giovanni-hislop/"
-          target="_blank"
-          title="My LinkedIn profile"
-        >
-          <RiLinkedinFill />
-        </a>
-
-        <a
-          className="flex items-center bg-[#d0cfc5] p-4 text-gray-500 hover:text-gray-950 gap-2 rounded-full cursor-pointer focus:scale-115 hover:scale-110 active:scale-105 borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/gmhislop"
-          target="_blank"
-          title="My GitHub profile"
-        >
-          <BsGithub />
-        </a>
-      </motion.div>
     </section>
   );
 };
