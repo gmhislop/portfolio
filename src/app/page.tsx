b51c760e1c5ad6d563366e73894201ba1110b9af
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2023-10-03 14:10:01 +0200

    [improvement] - remove About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Hero />
-      <About />
+      {/* <About /> */}
       <Projects />
       <Contact />
     </main>
